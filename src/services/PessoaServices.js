98ff422789a85898287b91057d1742f0833cde58
Edmir Lôbo <ejuniorls@outlook.com>
2025-01-23 22:22:19 -0300

    Aula 03

---
+++ src/services/PessoaServices.js
@@ -0,0 +1,10 @@
+/* eslint-disable indent */
+const Services = require('./Services.js');
+
+class PessoaServices extends Services {
+    constructor() {
+        super('Pessoa');
+    }
+}
+
+module.exports = PessoaServices;
